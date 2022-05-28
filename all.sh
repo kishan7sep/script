2dbed033c88fe94239c6002b7a0469536842549e
kishan7sep <67074550+kishan7sep@users.noreply.github.com>
2022-05-28 22:16:51 +0530

    kubectl and aws cli

---
+++ all.sh
@@ -0,0 +1,14 @@
+echo "[1] Kubectl\n[2] AWSCLI v2"
+read number
+
+if [ number == 1]; then
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+fi
+if [ number == 2]; then
+    sudo yum remove awscli -y
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    sudo ./aws/install -i /usr/local/aws-cli -b /usr/local/bin
+    export PATH=$PATH:/usr/local/bin/aw
+fi   
